cadcfd6f8d84ba45a6c3f806281bdf7599af1c8d
harshkumar2003 <harshk8876@gmail.com>
2026-02-09 13:56:30 +0530

    Added all the with the server

--- backend/admin-Service/src/main/java/com/resolvelt/AdminServiceApplication.java
+++ backend/admin-Service/src/main/java/com/resolvelt/AdminServiceApplication.java
@@ -2,8 +2,10 @@ package com.resolvelt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+
 public class AdminServiceApplication {
 
 	public static void main(String[] args) {
